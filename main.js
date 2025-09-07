d74ff976920c702d3e687f845caafcc5d46d6cc7
Rinmnzz <cri.pizarrom@duocuc.cl>
2025-09-07 20:41:43 -0300

    '70-09-2025 -3- Cambios en el formulario, se agrego  el descuento del 20% por ser de duoc, tambien se cambio el titulo

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nombreInput = document.getElementById('nombre');
-    const correoInput = document.getElementById('correo');
-    const contraseñaInput = document.getElementById('contraseña');
-    const signUpBtn = document.getElementById('signUp');
+    var nombreInput = document.getElementById('nombre');
+    var correoInput = document.getElementById('correo');
+    var contraseñaInput = document.getElementById('contraseña');
+    var edadInput = document.getElementById('edad');
+    var signUpBtn = document.getElementById('signUp');
 
-    const nombreError = document.getElementById('nombreError');
-    const correoError = document.getElementById('correoError');
-    const contraseñaError = document.getElementById('contraseñaError');
+
+    var nombreError = document.getElementById('nombreError');
+    var correoError = document.getElementById('correoError');
+    var contraseñaError = document.getElementById('contraseñaError');
+    var edadError = document.getElementById('edadError');
 
     signUpBtn.addEventListener('click', function () {
         // Limpiar errores anteriores
@@ -15,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const nombre = nombreInput.value.trim();
         const correo = correoInput.value.trim();
         const contraseña = contraseñaInput.value.trim();
+        const edad = edadInput.value.trim();
 
         let hayErrores = false;
 
@@ -41,10 +45,24 @@ document.addEventListener('DOMContentLoaded', function () {
             contraseñaError.innerText = 'Debe tener al menos 6 caracteres, una letra y un número.';
             hayErrores = true;
         }
+        if(edad===''){
+            edadError.innerText='la edad es obligatoria.';
+            hayErrores = true;
+
+        }else if (isNaN(edad) || parseInt(edad) < 18){
+            edadError.innerText = 'Eres menor de edad.';
+
+            hayErrores = true;
+        }
 
         if (!hayErrores) {
-            // Si no hay errores, puedes proceder con el registro
-            alert('Registro exitoso');
+            // Preparar el mensaje de registro
+            let mensaje = 'Registro exitoso';
+            if (correo.endsWith('@duocuc.cl')) {
+            mensaje += '\n¡Obtienes un 20% de descuento de por vida por tu correo @duocuc.cl!';
+            }
+            alert(mensaje); // Mostrar solo un alert
+            window.location.href = 'index.html'; // Redirigir después del alert
         }
     });
 
